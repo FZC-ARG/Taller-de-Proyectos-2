7e22a67d9716e7403fb7781e078d99084dddaf77
Argan <71340325@continental.edu.pe>
2025-10-11 23:30:59 -0500

    HU 2.2 - 2.3 Panel Docente con gardner , actualizacion de la visualizacion del test de gardner

--- Proyecto/Frontend/src/app/inicio-docentes/inicio-docentes.component.ts
+++ Proyecto/Frontend/src/app/inicio-docentes/inicio-docentes.component.ts
@@ -1,11 +1,54 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-inicio-docentes',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './inicio-docentes.component.html',
   styleUrl: './inicio-docentes.component.css'
 })
 export class InicioDocentesComponent {
 
+  seccionActual: 'alumnos' | 'test' = 'alumnos';
+
+  alumnos = [
+    'Juan',
+    'Pedro',
+    'Luis',
+    'Enrique',
+    'Carlos',
+    'Diego',
+    'Fernando',
+    'Gabriel',
+    'Miguel',
+    'Jose',
+    'Maria',
+    'Ana',
+    'María',
+    'Laura',
+    'Sofía',
+    'Pablo',
+    'Javier',
+    'Daniel',
+    'Ángel',
+    'Lucas',
+  ];
+
+  constructor(private router: Router) {}
+
+  mostrarSeccion(seccion: 'alumnos' | 'test') {
+    this.seccionActual = seccion;
+  }
+
+  irATestGardner() {
+    this.router.navigate(['/test-gardner']);
+  }
+
+  logout() {
+    localStorage.clear();
+    this.router.navigate(['/login']);
+  }
 }
+
