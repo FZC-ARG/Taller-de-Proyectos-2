0f7e04b574a0a14c261ea91c4c3cadb03a45c847
Argan <71340325@continental.edu.pe>
2025-11-05 13:47:54 -0500

    HU Chatbot interfaz  - obtencion de conversacion con la ia , envio de mensajes de la ia - implementacion de graficos

---
+++ Proyecto/Frontend/src/app/services/chatbot.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { API_URL } from '../conexion';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatbotService {
+
+  constructor(private http: HttpClient) { }
+
+  getSesion(idDocente: string, idAlumno: string): Observable<any> {
+    return this.http.get(`${API_URL}api/chat/sesiones/docente/${idDocente}/alumno/${idAlumno}/ultima`);
+  }
+
+  getMensajes(idSesion: string): Observable<any> {
+    return this.http.get(`${API_URL}api/chat/sesiones/${idSesion}/mensajes`);
+  }
+
+  enviarMensaje(idSesion: string , contenido: string): Observable<any> {
+    return this.http.post(`${API_URL}api/chat/sesiones/${idSesion}/mensajes`, { contenido });
+  } 
+}
